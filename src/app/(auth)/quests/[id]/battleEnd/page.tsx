40ae04e80e2c53ddf88e54d7333356d1c60fb3e0
三浦大河 <taigav8@gmail.com>
2024-09-11 21:13:02 +0900

    戦闘終了ページのレイアウト作成

--- src/app/(auth)/quests/[id]/battleEnd/page.tsx
+++ src/app/(auth)/quests/[id]/battleEnd/page.tsx
@@ -1,3 +1,33 @@
-export default function BattleEndPage() {
-  return <article>戦闘終了ページページ</article>;
-}
+"use client";
+
+import { BasicButton, XShareButton } from "@/components/layouts";
+import Image from "next/image";
+import Link from "next/link";
+
+const BattleEnd = () => {
+  return (
+    <div className="relative min-h-screen flex flex-col items-center justify-between p-4">
+      <Image
+        src={"/images/monsters/ends/monster1.jpg"}
+        alt="Battle End Background"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        className="-z-10"
+      />
+      <div className="absolute top-10 left-1/2 transform -translate-x-1/2 text-white text-4xl font-bold z-10">
+        <p className="text-2xl sm:text-2xl md:text-4xl bg-black bg-opacity-50 p-4 rounded-md mt-20 sm:mb-6">
+          討伐完了！掃除の成果をXに共有しよう！
+        </p>
+      </div>
+      <div className="w-full max-w-2xl flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-4 mt-auto mb-48 z-10">
+        <Link href={"/quests"}>
+          <BasicButton text="クエスト一覧" />
+        </Link>
+        <XShareButton text="Xに共有する" />
+      </div>
+    </div>
+  );
+};
+
+export default BattleEnd;
---
+++ src/components/layouts/xShareButton/index.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Button, ButtonProps, styled } from "@mui/material";
+
+const BlackStyledButton = styled(Button)({
+  backgroundColor: "#000",
+  color: "#fff",
+  fontWeight: "bold",
+  fontSize: "1.2rem",
+  padding: "12px 24px",
+  border: "2px solid #C0C0C0",
+  borderRadius: "10px",
+  textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+  boxShadow: "0 8px 16px rgba(0, 0, 0, 0.6)",
+  transition: "transform 0.2s, background-color 0.2s, box-shadow 0.2s",
+  "&:hover": {
+    backgroundColor: "#333",
+    boxShadow: "0 12px 24px rgba(0, 0, 0, 0.8)",
+    transform: "scale(1.05)",
+  },
+  "&:active": {
+    backgroundColor: "#111",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.8)",
+    transform: "scale(0.95)",
+  },
+});
+
+interface BasicButtonProps extends ButtonProps {
+  text: string;
+}
+
+const XShareButton: React.FC<BasicButtonProps> = ({ text, ...props }) => {
+  return <BlackStyledButton {...props}>{text}</BlackStyledButton>;
+};
+
+export default XShareButton;
