b9ea7ff49cc8dda6f573ae5b84c5ed3b7da1762d
三浦大河 <taigav8@gmail.com>
2024-10-16 15:27:47 +0900

    各ページのタイトル修正

--- src/app/(auth)/guildCards/encyclopedias/[uid]/page.tsx
+++ src/app/(auth)/guildCards/encyclopedias/[uid]/page.tsx
@@ -47,7 +47,10 @@ export default function MonsterEncyclopedia() {
   return (
     <div className="relative min-h-screen overflow-hidden bg-[url('/images/layouts/basic_background.jpg')] bg-auto bg-repeat">
       <div className="absolute left-1/2 top-10 z-10 -translate-x-1/2 text-4xl font-bold text-white">
-        <p className="mt-2 rounded-md bg-black/50 p-4 text-2xl sm:mb-2 sm:text-2xl md:text-5xl">
+        <p
+          className="mt-2 min-w-[300px] rounded-md bg-black/50 p-4 text-center text-2xl sm:mb-2 sm:text-2xl md:text-5xl"
+          style={{ whiteSpace: 'nowrap' }}
+        >
           モンスター図鑑
         </p>
       </div>
