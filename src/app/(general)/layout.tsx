ccad824baa8e5b7341490463bdc5af0f54514965
三浦大河 <taigav8@gmail.com>
2024-10-19 03:13:12 +0900

    fix-background_image

--- src/app/(general)/layout.tsx
+++ src/app/(general)/layout.tsx
@@ -1,17 +1,9 @@
-import Image from 'next/image';
 import { Headers, Footers } from '@/components/layouts';
 import { GeneralLayoutProps } from '@/types';
 
 export default function GeneralLayout({ children }: GeneralLayoutProps) {
   return (
     <div>
-      <Image
-        src="/images/layouts/top_page_background_image.jpg"
-        alt="Dust Hunters Background"
-        fill
-        style={{ objectFit: 'cover' }}
-        className="z-0"
-      />
       <Headers />
       <main>{children}</main>
       <Footers />
