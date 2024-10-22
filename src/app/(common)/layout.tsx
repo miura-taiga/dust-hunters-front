357b9e302e57c616e89d582e9ed3c9ce2e497c0b
三浦大河 <taigav8@gmail.com>
2024-10-22 17:48:40 +0900

    ESLintの修正

--- src/app/(common)/layout.tsx
+++ src/app/(common)/layout.tsx
@@ -10,7 +10,7 @@ export default function CommonLayout({ children }: CommonLayoutProps) {
         alt="Basic Background Image"
         fill
         style={{ objectFit: 'cover' }}
-        className="absolute left-0 top-0 -z-10 w-full h-full"
+        className="absolute left-0 top-0 -z-10 size-full"
       />
       <Headers />
       <main className="relative z-10 grow">{children}</main>
--- src/app/(common)/termOfService/page.tsx
+++ src/app/(common)/termOfService/page.tsx
@@ -1,6 +1,6 @@
 export default function TermOfService() {
   return (
-    <div className="relative min-h-screen flex flex-col items-center justify-center">
+    <div className="relative flex min-h-screen flex-col items-center justify-center">
       <div className="absolute left-1/2 top-4 z-10 -translate-x-1/2 text-4xl font-bold text-white">
         <p
           className="mt-2 min-w-[300px] rounded-md bg-black/50 p-4 text-center text-2xl sm:mb-2 sm:text-2xl md:text-5xl"
@@ -9,10 +9,10 @@ export default function TermOfService() {
           利用規約
         </p>
       </div>
-      <div className="z-10 mt-32 mb-10 w-full max-w-4xl p-8 bg-black/60 text-white rounded-lg shadow-lg">
+      <div className="z-10 mb-10 mt-32 w-full max-w-4xl rounded-lg bg-black/60 p-8 text-white shadow-lg">
         {/* 1. はじめに */}
         <section className="mb-8">
-          <h2 className="text-xl font-bold mb-4">1. はじめに</h2>
+          <h2 className="mb-4 text-xl font-bold">1. はじめに</h2>
           <p className="text-gray-200">
             本利用規約（以下、「本規約」）は、<strong>DustHunters</strong>
             （以下、「本アプリ」）を提供する三浦大河（以下、「当社」）が、本アプリの利用条件を定めるものです。利用者（以下、「ユーザー」）は、本規約に同意した上で本アプリを利用するものとします。
@@ -21,8 +21,8 @@ export default function TermOfService() {
 
         {/* 2. アカウントの作成と管理 */}
         <section className="mb-8">
-          <h2 className="text-xl font-bold mb-4">2. アカウントの作成と管理</h2>
-          <ul className="list-disc list-inside text-gray-200">
+          <h2 className="mb-4 text-xl font-bold">2. アカウントの作成と管理</h2>
+          <ul className="list-inside list-disc text-gray-200">
             <li>
               本アプリを利用するためには、ユーザーはアカウントを作成する必要があります。
             </li>
@@ -37,11 +37,11 @@ export default function TermOfService() {
 
         {/* 3. 禁止事項 */}
         <section className="mb-8">
-          <h2 className="text-xl font-bold mb-4">3. 禁止事項</h2>
+          <h2 className="mb-4 text-xl font-bold">3. 禁止事項</h2>
           <p className="text-gray-200">
             ユーザーは以下の行為を行ってはなりません。
           </p>
-          <ul className="list-disc list-inside text-gray-200 mt-2">
+          <ul className="mt-2 list-inside list-disc text-gray-200">
             <li>違法行為や犯罪行為の助長</li>
             <li>当社または第三者の知的財産権を侵害する行為</li>
             <li>他のユーザーのプライバシーを侵害する行為</li>
@@ -53,7 +53,7 @@ export default function TermOfService() {
 
         {/* 4. 免責事項 */}
         <section className="mb-8">
-          <h2 className="text-xl font-bold mb-4">4. 免責事項</h2>
+          <h2 className="mb-4 text-xl font-bold">4. 免責事項</h2>
           <p className="text-gray-200">
             当社は、本アプリがユーザーの特定の目的に適合することや、エラーや中断がないことを保証するものではありません。
             本アプリの利用により生じたいかなる損害に対しても、当社は一切の責任を負いません。また、当社は、第三者による不正行為や本規約の範囲外で生じた損害に対して責任を負いません。
@@ -62,7 +62,7 @@ export default function TermOfService() {
 
         {/* 5. 知的財産権 */}
         <section className="mb-8">
-          <h2 className="text-xl font-bold mb-4">5. 知的財産権</h2>
+          <h2 className="mb-4 text-xl font-bold">5. 知的財産権</h2>
           <p className="text-gray-200">
             本アプリに関連するすべてのコンテンツ、ソフトウェア、技術、商標、ロゴ、その他の知的財産権は、当社またはそのライセンサーに帰属します。
             ユーザーは、当社の事前の書面による同意なく、これらの知的財産権を使用、複製、配布、改変することはできません。
@@ -71,7 +71,7 @@ export default function TermOfService() {
 
         {/* 6. プライバシーポリシー */}
         <section className="mb-8">
-          <h2 className="text-xl font-bold mb-4">6. プライバシーポリシー</h2>
+          <h2 className="mb-4 text-xl font-bold">6. プライバシーポリシー</h2>
           <p className="text-gray-200">
             本アプリにおける個人情報の取り扱いについては、別途定めるプライバシーポリシーに従うものとします。
           </p>
@@ -79,7 +79,7 @@ export default function TermOfService() {
 
         {/* 7. 利用の停止および契約解除 */}
         <section className="mb-8">
-          <h2 className="text-xl font-bold mb-4">
+          <h2 className="mb-4 text-xl font-bold">
             7. 利用の停止および契約解除
           </h2>
           <p className="text-gray-200">
@@ -90,7 +90,7 @@ export default function TermOfService() {
 
         {/* 8. 規約の変更 */}
         <section className="mb-8">
-          <h2 className="text-xl font-bold mb-4">8. 規約の変更</h2>
+          <h2 className="mb-4 text-xl font-bold">8. 規約の変更</h2>
           <p className="text-gray-200">
             当社は、必要に応じて本規約を変更できるものとします。変更後の規約は、本アプリ上で通知された時点で効力を持つものとします。
             ユーザーが変更後も本アプリを利用した場合、変更内容に同意したものとみなされます。
@@ -99,14 +99,14 @@ export default function TermOfService() {
 
         {/* 9. 準拠法および裁判管轄 */}
         <section className="mb-8">
-          <h2 className="text-xl font-bold mb-4">9. 準拠法および裁判管轄</h2>
+          <h2 className="mb-4 text-xl font-bold">9. 準拠法および裁判管轄</h2>
           <p className="text-gray-200">
             本規約は、日本法に基づいて解釈されます。本規約に関して生じた紛争については、札幌地方裁判所を専属的な管轄裁判所とします。
           </p>
         </section>
 
         {/* 最終更新日など */}
-        <div className="text-gray-400 text-sm mt-8">
+        <div className="mt-8 text-sm text-gray-400">
           <p>制定日: 2024年10月22日</p>
           <p>最終更新日: 2024年10月22日</p>
         </div>
